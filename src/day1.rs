b5441769d2e744700c1f9c6ae1de5c3c60b90a17
geeshta <stefan.foldesi@gopay.cz>
2025-12-02 15:14:43 +0100

    feat: day1 part2 solution

--- src/day1.rs
+++ src/day1.rs
@@ -1,42 +1,89 @@
-const ERROR_MESSAGE: &'static str = "Inputs must start with L/R followed by numerals.";
+const DAY1_ERR_MSG: &str = "Inputs must start with L/R followed by numerals.";
 
-fn parse_step(s: &str) -> i64 {
+/// Strip prefix and convert to an dial instruction as a signed number
+/// Panics on invalid inputs
+fn parse_step(s: &str) -> isize {
     if let Some(rest) = s.strip_prefix("L") {
-        -rest.parse::<i64>().expect(ERROR_MESSAGE)
+        -rest.parse::<isize>().expect(DAY1_ERR_MSG)
     } else if let Some(rest) = s.strip_prefix("R") {
-        rest.parse::<i64>().expect(ERROR_MESSAGE)
+        rest.parse::<isize>().expect(DAY1_ERR_MSG)
     } else {
-        panic!("{}", ERROR_MESSAGE)
+        panic!("{}", DAY1_ERR_MSG)
     }
 }
 
-pub fn day1_part1_func(input: &[&str]) -> i64 {
+/// The number of times dial lands exactly on 0. Pure functional solution.
+pub fn day1_part1_func(input: &[&str]) -> isize {
     input
         .iter()
-        .fold((50, 0), |acc: (i64, i64), s: &&str| {
+        .fold((50, 0), |acc: (isize, isize), s: &&str| {
             let step = parse_step(s);
+            let tally_mod = (acc.0 + step).rem_euclid(100);
+            let inc = (tally_mod == 0) as isize;
 
-            let tally = acc.0 + step;
-            if tally.rem_euclid(100) == 0 {
-                (tally, acc.1 + 1)
-            } else {
-                (tally, acc.1)
-            }
+            (tally_mod, acc.1 + inc)
         })
         .1
 }
 
-pub fn day1_part1_imp(input: &[&str]) -> i64 {
+/// The number of times dial lands exactly on 0. Idiomatic imperative solution.
+pub fn day1_part1_imp(input: &[&str]) -> isize {
     let mut tally = 50;
     let mut zeroes = 0;
 
     for s in input {
         let step = parse_step(s);
-
         tally += step;
+
         if tally.rem_euclid(100) == 0 {
             zeroes += 1;
         }
+
+        tally = tally.rem_euclid(100);
+    }
+    zeroes
+}
+
+/// Count how many times 0 has been crossed by comparing whether after a step, a value becomes
+/// negative (crossed to the left) or over a 100 (crossed from the right)
+fn zero_crosses(step: isize, tally: isize) -> isize {
+    let hundreds = (tally / 100).abs();
+
+    let adj = (tally - step == 0) as isize;
+
+    match step {
+        st if st > 0 => hundreds,
+        st if st < 0 => (tally < 1).then_some(hundreds + 1 - adj).unwrap_or(0),
+        _ => 0,
+    }
+}
+
+/// The number of times dial crosses 0. Pure functional solution.
+pub fn day1_part2_func(input: &[&str]) -> isize {
+    input
+        .iter()
+        .fold((50, 0), |acc: (isize, isize), s: &&str| {
+            let step = parse_step(s);
+            let tally = acc.0 + step;
+            let inc = zero_crosses(step, tally) as isize;
+
+            (tally.rem_euclid(100), acc.1 + inc)
+        })
+        .1
+}
+
+/// The number of times dial crosses 0. Idiomatic imperative solution.
+pub fn day1_part2_imp(input: &[&str]) -> isize {
+    let mut tally = 50;
+    let mut zeroes = 0;
+
+    for s in input {
+        let step = parse_step(s);
+        tally += step;
+
+        zeroes += zero_crosses(step, tally);
+
+        tally = tally.rem_euclid(100);
     }
     zeroes
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod day1;
-use day1::{day1_part1_func, day1_part1_imp};
+use day1::*;
 
-const DAY1_INPUT: &str = include_str!("../inputs/day1_part1.csv");
+const DAY1_INPUT: &str = include_str!("../inputs/day1_test.csv");
 
 fn main() {
     let day1_input: Vec<&'static str> = DAY1_INPUT.lines().collect();
@@ -10,5 +10,11 @@ fn main() {
     println!("{:?}", password_func);
 
     let password_imp = day1_part1_imp(&day1_input);
+    println!("{:?}", password_imp);
+
+    let password_func = day1_part2_func(&day1_input);
+    println!("{:?}", password_func);
+
+    let password_imp = day1_part2_imp(&day1_input);
     println!("{:?}", password_imp);
 }
